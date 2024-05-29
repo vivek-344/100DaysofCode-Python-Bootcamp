8ec4b559cddb340140cc3bb25233868c352f28f7
Vivekraj Singh Sisodiya <vivekuit344@gmail.com>
2024-05-29 14:18:11 +0530

    Day 6

---
+++ day6.1-escaping-the-maze.py
@@ -0,0 +1,24 @@
+# Below is the solution code to escape the maze that the Reeborg is stuck in.
+# Link to the maze:
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+"""
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+"""
+
+# Reeborg follow along the right edge of the maze,
+# turning right if it can, going straight ahead
+# if it can’t turn right, or turning left as a last resort.
