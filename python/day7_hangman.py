a8cf81d70954fa0126a6dce13f04ce970b8d58ee
Vivekraj Singh Sisodiya <vivekuit344@gmail.com>
2024-05-30 12:49:33 +0530

    Day 7 (null input bug fixed)

--- python/day7_hangman.py
+++ python/day7_hangman.py
@@ -4,6 +4,9 @@ from day7_hangman_words import words
 from day7_hangman_art import logo
 from day7_hangman_art import stages
 
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z']
+
 word_list = words.split()
 random_word = random.choice(word_list)
 
@@ -18,14 +21,20 @@ print(logo)
 
 while lives != 0 and '_' in answer:
     ch = input("Guess a letter: ").strip().lower()
-    print("\n")
-    if ch[0] in answer:
+    if ch not in letters:
+        print("Invalid input..", end=" ")
+        continue
+    elif ch[0] in answer:
+        print("\n")
         print(f"You already guessed {ch[0]}")
+        continue
     elif ch[0] in random_word:
+        print("\n")
         for i in range(0, len(answer)):
             if random_word[i] == ch:
                 answer[i] = ch
     else:
+        print("\n")
         lives -= 1
         print("You lost a life.")
     for ch in answer:
@@ -36,4 +45,4 @@ while lives != 0 and '_' in answer:
 if lives > 0:
     print("You Win!")
 else:
-    print("You lose.")
+    print(f"You lose. The word was {random_word}")
