1edf2eac8492321ab4583dcfdaf9d25aef3bb1bb
Vivekraj Singh Sisodiya <vivekuit344@gmail.com>
2024-06-26 15:43:28 +0530

    Day 30

--- python/day 20-30/day 26/nato_alphabet.py
+++ python/day 20-30/day 26/nato_alphabet.py
@@ -4,8 +4,29 @@ data = pd.read_csv("nato_phonetics.csv")
 
 phonetics = {row.letter: row.code for (index, row) in data.iterrows()}
 
-word = input("Enter a word: ").upper()
+# loop_flag = True
+#
+# while loop_flag:
+#     try:
+#         word = input("Enter a word: ").upper()
+#         phonetic_list = [phonetics[letter] for letter in word]
+#     except KeyError:
+#         print("Please enter only alphabets.")
+#     else:
+#         loop_flag = False
+#         print(phonetic_list)
 
-phonetic_list = [phonetics.get(letter) for letter in word]
 
-print(phonetic_list)
+# Recursive Approach
+def generate_phonetics():
+    word = input("Enter a word: ").upper()
+    try:
+        phonetic_list = [phonetics[letter] for letter in word]
+    except KeyError:
+        print("Please enter only alphabets.")
+        generate_phonetics()
+    else:
+        print(phonetic_list)
+
+
+generate_phonetics()
--- python/day 20-30/day 29-30/password_manager.py
+++ python/day 20-30/day 29-30/password_manager.py
@@ -3,6 +3,7 @@ from tkinter import *
 import pyperclip
 import string
 import random
+import json
 
 
 def generate_password():
@@ -28,6 +29,12 @@ def add_entry():
     web = website.get()
     email = username.get()
     passcode = password.get()
+    data = {
+        web: {
+            "email": email,
+            "password": passcode
+        }
+    }
 
     if len(web) == 0 or len(passcode) == 0 or len(email) == 0:
         messagebox.showinfo(title="Empty Fields", message="You cannot add empty fields.")
@@ -36,10 +43,45 @@ def add_entry():
                                                                  f"{email}\nPassword = {passcode}")
 
         if confirmation:
-            with open(file="passwords.txt", mode="a") as file:
-                file.write(f"{web} | {email} | {passcode}\n")
+
+            content = {}
+
+            # txt format file
+            # with open(file="passwords.txt", mode="a") as file:
+            #     file.write(f"{web} | {email} | {passcode}\n")
+
+            # json format file
+            try:
+                with open(file="passwords.json", mode="r") as file:
+                    content = json.load(file)
+                    content.update(data)
+            except FileNotFoundError:
+                content = data
+            finally:
+                with open(file="passwords.json", mode="w") as file:
+                    json.dump(content, file, indent=4)
                 website.delete(0, END)
                 password.delete(0, END)
+
+
+def search():
+    web = website.get()
+    try:
+        with open(file="passwords.json", mode="r") as file:
+            content = json.load(file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="No Entry Found", message=f'There is no entry for "{web}" website.')
+    else:
+        if len(web) == 0:
+            messagebox.showinfo(title="Empty Field", message="Enter the website name to search for the data.")
+        else:
+            try:
+                data = content[web]
+            except KeyError:
+                messagebox.showinfo(title="No Entry Found", message=f'There is no entry for "{web}" website.')
+            else:
+                messagebox.showinfo(title=f"{web}", message=f'email = {data["email"]}\npassword = {data["password"]}')
+                pyperclip.copy(data["password"])
 
 
 window = Tk()
@@ -60,9 +102,12 @@ label.grid(row=2, column=0, sticky="e")
 label = Label(text="Password:")
 label.grid(row=3, column=0, sticky="e")
 
-website = Entry(width=36)
-website.grid(row=1, column=1, columnspan=2, pady=5)
+website = Entry(width=18)
+website.grid(row=1, column=1, pady=5)
 website.focus()
+
+search = Button(text="Search", command=search, width=12)
+search.grid(row=1, column=2)
 
 username = Entry(width=36)
 username.insert(END, string="vivekrajsingh344@gmail.com")
