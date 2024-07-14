9b66b22264a2ea03e9ab359aec2d25960ab82fb1
Vivekraj Singh Sisodiya <vivekuit344@gmail.com>
2024-07-14 23:30:01 +0530

    Day 46

--- python/day 41-50/day 46/spotify_playlist_creator.py
+++ python/day 41-50/day 46/spotify_playlist_creator.py
@@ -1,11 +1,19 @@
+import os
+from spotipy.oauth2 import SpotifyOAuth
 from bs4 import BeautifulSoup
 from datetime import datetime
 import requests
+import spotipy
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_KEY = os.getenv("CLIENT_SECRET")
 
 
 def is_valid_date(date_str):
     try:
-        # Try to parse the date string with the format YYYY-MM-DD
         datetime.strptime(date_str, '%Y-%m-%d')
         return True
     except ValueError:
@@ -25,10 +33,36 @@ soup = BeautifulSoup(response.text, "html.parser")
 
 html_list = soup.find_all(class_="o-chart-results-list-row-container")
 
-song_dict = {}
+song_list = []
 
 for item in html_list:
-    song_dict[item.select_one("div ul li ul li span").text.strip()] = item.select_one("div ul li ul li h3").text.strip()
+    song_list.append(item.select_one("div ul li ul li h3").text.strip())
 
-for key, value in song_dict.items():
-    print(f"{key}: {value}")
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_KEY,
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
+print(user_id)
+
+song_uris = []
+year = date.split("-")[0]
+for song in song_list:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+print(playlist)
+
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
