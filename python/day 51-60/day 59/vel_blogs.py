a21499773a30f375d4f204c8fad1ed75491863f2
Vivekraj Singh Sisodiya <vivekuit344@gmail.com>
2024-07-30 18:51:55 +0530

    Day 60

--- python/day 51-60/day 59/vel_blogs.py
+++ python/day 51-60/day 59/vel_blogs.py
@@ -1,8 +1,16 @@
+import os
 import requests
 import datetime
-from flask import Flask, render_template
+import smtplib
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, jsonify
 
+load_dotenv()
 app = Flask(__name__)
+
+
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
 
 
 @app.route('/')
@@ -25,7 +33,7 @@ def blog(post_id):
     posts = requests.get("https://api.npoint.io/58953a10e49990b48467").json()
     post = next((p for p in posts if p["id"] == post_id), None)
     if not post:
-        return page_not_found(404)  # Call the error handler directly
+        return page_not_found(404)
     return render_template("post.html", year=datetime.datetime.now().year, **post)
 
 
@@ -39,6 +47,34 @@ def about():
 def contact():
     year = datetime.datetime.now().year
     return render_template("contact.html", year=year)
+
+
+@app.route('/send_mail', methods=["POST"])
+def send_mail():
+    try:
+        data = request.get_json()
+        if not data:
+            raise ValueError("No data received")
+
+        if request.method == 'POST':
+            with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+                connection.starttls()
+                connection.login(EMAIL, PASSWORD)
+                connection.sendmail(
+                    EMAIL,
+                    EMAIL,
+                    msg=f"Subject: New Contact Form Submission\n\n"
+                        f"Name: {data['name']}\n"
+                        f"Email: {data['email']}\n"
+                        f"Phone: {data['phone']}\n"
+                        f"Message: {data['message']}"
+                )
+
+        return jsonify({"message": "Form submission successful!"}), 200
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"message": "Error processing the form submission."}), 500
 
 
 @app.errorhandler(404)
